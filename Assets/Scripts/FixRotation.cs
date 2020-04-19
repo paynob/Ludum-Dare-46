19a5e50bcb72092d9706c4d3f708d0d81d0de41e
Payno <payno@concanogames.com>
2020-04-19 19:31:19 +0200

    Updated level01

---
+++ Assets/Scripts/FixRotation.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class FixRotation : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.identity;
+    }
+}
