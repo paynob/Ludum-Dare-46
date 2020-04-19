9f675e59fa91e1cc8ccd152a852b171adb2a62d1
juancarlos.fernandez.conde.next <juancarlos.fernandez.conde.next@bbva.com>
2020-04-19 20:38:06 +0200

    feat(sprites): moving diamonds and gas is now gas

--- Assets/Scripts/Diamond.cs
+++ Assets/Scripts/Diamond.cs
@@ -3,6 +3,16 @@
 [RequireComponent(typeof(Collider2D))]
 public class Diamond : MonoBehaviour
 {
+    public ParticleSystem particles;
+    public SpriteRenderer spriteRenderer;
+
+    private void Awake () {
+        if (particles != null) {
+            ParticleSystem.MainModule mainModule = particles.main;
+            mainModule.startColor = spriteRenderer.color;
+        }
+    }
+
     private void OnTriggerEnter2D( Collider2D collision )
     {
         if ( collision.CompareTag("Player") )
